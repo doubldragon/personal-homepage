ea83f01151bcb3c7b504abaa6190c9155b812ea6
Brandon Spencer <brandon.spencer@gmail.com>
2026-04-12 17:38:55 -0400

    update content in projects and experience

--- data/projects.ts
+++ data/projects.ts
@@ -19,6 +19,56 @@ const projects: Project[] = [
     year: 2019,
     link: "",
   },
+  {
+    filename: null,
+    title: "Injection Molding Auto-quote",
+    blurb:
+      "Frontend Alarm dashboard ingesting large datasets and optimizing performance",
+    category: "Xometry",
+    skills: ["React", "Typescript", "Python", "AWS"],
+    year: 2024,
+    link: "",
+  },
+  {
+    filename: null,
+    title: "Teamspace",
+    blurb:
+      "Frontend Alarm dashboard ingesting large datasets and optimizing performance",
+    category: "Xometry",
+    skills: ["React", "Typescript", "Python", "AWS"],
+    year: 2023,
+    link: "",
+  },
+  {
+    filename: null,
+    title: "Wilcutt Guitars",
+    blurb:
+      "Frontend Alarm dashboard ingesting large datasets and optimizing performance",
+    category: "Fusioncorp",
+    skills: ["Symfony", "PHP", "React", "AWS", "Postgres"],
+    year: 2018,
+    link: "",
+  },
+  {
+    filename: null,
+    title: "Manufacturing Region Control",
+    blurb:
+      "Frontend Alarm dashboard ingesting large datasets and optimizing performance",
+    category: "Xometry",
+    skills: ["React", "Typescript", "Python", "AWS"],
+    year: 2024,
+    link: "",
+  },
+  {
+    filename: null,
+    title: "Kentucky Injury Prevention and Research Center",
+    blurb:
+      "Frontend Alarm dashboard ingesting large datasets and optimizing performance",
+    category: "Apax Software",
+    skills: ["Django", "Python", "React", "MySql"],
+    year: 2021,
+    link: "",
+  },
 ];
 
 export default projects;
